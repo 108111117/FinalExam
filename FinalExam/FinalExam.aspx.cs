65a51b176fe4c129ebcbae91df4d68b58a28e823
丞展 劉 <108111117@mail.aeust.edu.tw>
2022-01-09 14:26:43 +0800

    202201090222

--- FinalExam/FinalExam.aspx.cs
+++ FinalExam/FinalExam.aspx.cs
@@ -7,9 +7,12 @@ using System.Web.UI.WebControls;
 
 namespace FinalExam
 {
+
     public partial class FinalExam : System.Web.UI.Page
     {
-        string[,] sa_Place = new string[2, 3] { { "基隆", "台北", "新北" }, { "苗栗", "台中", "南投" } };
+        string[,] sa_Place = new string[3, 6] { { "基隆", "台北", "新北" ,"桃園","宜蘭","新竹"},
+                                                { "苗栗", "台中", "南投" ,"彰化","雲林","花蓮"},
+                                                { "嘉義", "台南", "高雄" ,"屏東","台東",null} };
         protected void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack == false)
@@ -18,8 +21,10 @@ namespace FinalExam
                 ddl_Area.Items.Add(o_Item);
                 o_Item = new ListItem("中區", "中區");
                 ddl_Area.Items.Add(o_Item);
+                o_Item = new ListItem("南區", "南區");
+                ddl_Area.Items.Add(o_Item);
 
-                for (int i_ct = 0; i_ct < 3; i_ct++)
+                for (int i_ct = 0; i_ct < 6; i_ct++)
                 {
                     o_Item = new ListItem(sa_Place[0, i_ct], sa_Place[0, i_ct]);
                     ddl_Place.Items.Add(o_Item);
@@ -27,24 +32,31 @@ namespace FinalExam
 
             }
         }
-
         protected void ddl_Area_SelectedIndexChanged(object sender, EventArgs e)
         {
             ddl_Place.Items.Clear();
             ListItem o_Item;
             if (ddl_Area.SelectedValue == "北區")
             {
-                for (int i_ct = 0; i_ct < 3; i_ct++)
+                for (int i_ct = 0; i_ct < 6; i_ct++)
                 {
                     o_Item = new ListItem(sa_Place[0, i_ct], sa_Place[0, i_ct]);
                     ddl_Place.Items.Add(o_Item);
                 }
             }
-            else
+            else if (ddl_Area.SelectedValue == "中區")
             {
-                for (int i_ct = 0; i_ct < 3; i_ct++)
+                for (int i_ct = 0; i_ct < 6; i_ct++)
                 {
                     o_Item = new ListItem(sa_Place[1, i_ct], sa_Place[1, i_ct]);
+                    ddl_Place.Items.Add(o_Item);
+                }
+            }
+            else
+            {
+                for (int i_ct = 0; i_ct < 6; i_ct++)
+                {
+                    o_Item = new ListItem(sa_Place[2, i_ct], sa_Place[2, i_ct]);
                     ddl_Place.Items.Add(o_Item);
                 }
             }
