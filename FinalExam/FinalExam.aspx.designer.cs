59a4c9eccd02cb9c5309580f6270e22d65ddaf38
丞展 劉 <108111117@mail.aeust.edu.tw>
2022-01-09 15:28:20 +0800

    20220109327

--- FinalExam/FinalExam.aspx.designer.cs
+++ FinalExam/FinalExam.aspx.designer.cs
@@ -40,5 +40,14 @@ namespace FinalExam
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList ddl_Place;
+
+        /// <summary>
+        /// Btn_Search 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Btn_Search;
     }
 }
